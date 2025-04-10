2213e63599a9c04e0c6c0a7e5727be1931f6cbca
Amr4x4 <amranwarx2@gmail.com>
2025-04-10 18:01:52 +0200

    Improving in Splash Screen

--- app/src/main/java/com/example/azhealthcare/onboarding/OnBoardingScreen.kt
+++ app/src/main/java/com/example/azhealthcare/onboarding/OnBoardingScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.azhealthcare.R
 import com.example.azhealthcare.common_ui.BackgroundScreen
@@ -103,4 +104,10 @@ fun OnboardingScreen(
             }
         }
     }
+}
+
+@Preview
+@Composable
+private fun OnBoardingPreview() {
+    OnboardingScreen(navController =  NavController())
 }
